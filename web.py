99dc8d5911474b0ca38de896305cc2048d1e9a56
HyunSu Lim <bookman1209@gmail.com>
2015-06-02 05:07:26 +0900

    Leading zeros in encrypted/decrypted results are now correctly returned

--- web.py
+++ web.py
@@ -9,7 +9,9 @@ def encrypt(hex_plain, key, bits):
         hex_plain += '0' * (32 - len(hex_plain) % 32)
     result = str()
     for i in range( len(hex_plain)//32 ):
-        result += hex( ARIA.ARIA_encryption( int( hex_plain[i*32: (i+1)*32], 16 ), key, bits ) )[2:]
+        encrypt = hex( ARIA.ARIA_encryption( int( hex_plain[i*32: (i+1)*32], 16 ), key, bits ) )[2:]
+        encrypt = '0' * (32 - len(encrypt)) + encrypt
+        result += encrypt
     return result
 
 @app.route('/decrypt/<string:hex_cipher>/<int:key>/<int:bits>/')
@@ -18,7 +20,9 @@ def decrypt(hex_cipher, key, bits):
         hex_cipher += '0' * (32 - len(hex_cipher) % 32)
     result = str()
     for i in range( len(hex_cipher)//32 ):
-        result += hex( ARIA.ARIA_decryption( int( hex_cipher[i*32: (i+1)*32], 16 ), key, bits ) )[2:]
+        decrypt = hex( ARIA.ARIA_decryption( int( hex_cipher[i*32: (i+1)*32], 16 ), key, bits ) )[2:]
+        decrypt = '0' * (32 - len(decrypt)) + decrypt
+        result += decrypt
     return result
 
 if __name__ == '__main__':
