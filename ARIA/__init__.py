4301a223ff93e1b648b8bf64eed42781ad3dc890
HyunSu Lim <bookman1209@gmail.com>
2015-06-02 20:03:10 +0900

    Minor changes (names are more expository) & Removing redundants

--- ARIA/__init__.py
+++ ARIA/__init__.py
@@ -1,4 +1,4 @@
-from .ARIA_Byte import *
+from . import ARIA_Byte as ARIA
 
 __version__ = "1.0.0"
 __all__ = ['ARIA_encryption', 'ARIA_decryption', 'toggleprint']
@@ -41,24 +41,24 @@ def ARIA_encryption(plain, key, bits):
         return
     byte_plain = int_to_byte(plain, 16)
     byte_key = int_to_byte(key, bits//8)
-    byte_roundkeys = KeyExpansion(byte_key)
-    byte_cipher = cipher(byte_plain, byte_roundkeys, printIntermediate)
+    byte_roundkeys = ARIA.KeyExpansion(byte_key)
+    byte_cipher = ARIA.cipher(byte_plain, byte_roundkeys, printIntermediate)
     return byte_to_int(byte_cipher)
 
-def ARIA_decryption(c, key, bits):
+def ARIA_decryption(cipher, key, bits):
     if bits not in [128, 192, 256]:
         print("Key length must be 128/192/256bits!")
         return
-    c_len = len(hex(c)[2:])
-    if  c_len > 32:
+    cipher_len = len(hex(cipher)[2:])
+    if  cipher_len > 32:
         print("Cipher text should be 128bits!")
         return
     key_len = len(hex(key)[2:])
     if key_len > bits//4:
         print("Key should be {0:}bits!".format(bits))
         return
-    byte_c = int_to_byte(c, 16)
+    byte_cipher = int_to_byte(c, 16)
     byte_key = int_to_byte(key, bits//8)
-    byte_roundkeys = DecKeyExpansion(byte_key)
-    byte_plain = cipher(byte_c, byte_roundkeys, printIntermediate)
+    byte_roundkeys = ARIA.DecKeyExpansion(byte_key)
+    byte_plain = ARIA.cipher(byte_cipher, byte_roundkeys, printIntermediate)
     return byte_to_int(byte_plain)
