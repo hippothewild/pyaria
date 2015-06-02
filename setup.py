2ba413c78765c8b634d1d67f733e291deb3855d4
HyunSu Lim <bookman1209@gmail.com>
2015-06-02 20:33:45 +0900

    Packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = "PyARIA",
+      version = "1.0.0",
+      description = "ARIA implementation with Python",
+      url = "https://github.com/jihwan0321/pyaria",
+      packages = ["ARIA"],
+      )
